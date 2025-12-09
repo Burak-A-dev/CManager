ceda11f2a70680e531c1e370a4c423252a7dc548
Burak-A-dev <alpay.ozdemirok@gmail.com>
2025-12-09 13:36:05 +0100

    Customer Model completed.

---
+++ CManager.Presentation.ConsoleApp/Models/CustomerModel.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+namespace CManager.Presentation.ConsoleApp.Models;
+
+internal class CustomerModel
+{
+    public int ID { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string PostalCode { get; set; } = string.Empty;
+    public string Town { get; set; } = string.Empty;
+
+}
