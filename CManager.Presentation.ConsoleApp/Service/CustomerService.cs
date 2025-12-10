0eac48b4c0afc74f163805610198796d5d9f915a
Burak-A-dev <alpay.ozdemirok@gmail.com>
2025-12-10 21:43:48 +0100

    initial commit of the customer service.

---
+++ CManager.Presentation.ConsoleApp/Service/CustomerService.cs
@@ -0,0 +1,30 @@
+﻿
+
+using CManager.Presentation.ConsoleApp.Interface;
+using CManager.Presentation.ConsoleApp.Models;
+
+namespace CManager.Presentation.ConsoleApp.Service;
+
+internal class CustomerService : ICustomerService
+
+{
+    public void Create(CustomerModel customer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<CustomerModel> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public CustomerModel GetById(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
