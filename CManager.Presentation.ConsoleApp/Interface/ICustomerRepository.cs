bc4e65a7393498503627f2a34a0b8c7d9181ed62
Burak-A-dev <alpay.ozdemirok@gmail.com>
2026-01-03 22:41:56 +0100

    interface completed for the repository

---
+++ CManager.Presentation.ConsoleApp/Interface/ICustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using CManager.Presentation.ConsoleApp.Models;
+
+namespace CManager.Presentation.ConsoleApp.Interface;
+
+internal interface ICustomerRepository
+{
+    // Spara en lista av kunder till en JSON-fil.
+    List<Customer> GetAll();
+
+    Customer GetById(string id);    
+
+    void AddCustomer(Customer customer);    
+
+    void SaveCustomerToJson(string filePath);
+
+    // Hämta kunder från fil och lägga dem i en lista.
+    List<Customer> GetCustomersFromFile(string filePath);
+
+}
