10edae4e0bb5e0faa157572e047a001c95458ef4
Burak-A-dev <alpay.ozdemirok@gmail.com>
2026-01-05 00:29:49 +0100

    repository progress

---
+++ CManager.Presentation.ConsoleApp/Repository/CustomerRepository.cs
@@ -0,0 +1,35 @@
+﻿using CManager.Presentation.ConsoleApp.Interface;
+using CManager.Presentation.ConsoleApp.Models;
+
+
+namespace CManager.Presentation.ConsoleApp.Repository;
+
+internal class CustomerRepository : ICustomerRepository
+{
+    private List<Customer> customers = new List<Customer>();
+
+    public void AddCustomer(Customer customer)
+    {
+        customers.Add(customer);
+    }
+
+    public List<Customer> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Customer GetById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Customer> GetCustomersFromFile(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveCustomerToJson(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+}
