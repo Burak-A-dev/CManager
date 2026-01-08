990a7e2aa11290054d9bd7ccfc7938c58972df03
Burak-A-dev <alpay.ozdemirok@gmail.com>
2026-01-08 20:52:39 +0100

    if statement added to the repository to check if the json file is null or empty.

--- CManager.Presentation.ConsoleApp/Repository/CustomerRepository.cs
+++ CManager.Presentation.ConsoleApp/Repository/CustomerRepository.cs
@@ -23,7 +23,15 @@ internal class CustomerRepository : ICustomerRepository
         // Copilot föreslog denna ändring från det tidigare 'List<Customer> deSerializedFile = JsonSerializer.Deserialize(fromFile);
         // Det jag förstår från detta är att koden inte vet vad Json filen ska Deserialiseras till om '<List<Customer>>' inte finns med.
         // Kort sagt så behövs en typ som den vill konvertera till, detta skiljer från Serialize vilket inte behöver någon typ.
-        List<Customer> deSerializedFile = JsonSerializer.Deserialize<List<Customer>>(fromFile);
+        if (fromFile == "" & fromFile) ;
+        {
+            return null;
+        }  else
+        {
+
+            List<Customer> deSerializedFile = JsonSerializer.Deserialize<List<Customer>>(fromFile);
+        }
+
 
         // behöver lägga till en if sats för att se om filen är tom eller inte.
 
